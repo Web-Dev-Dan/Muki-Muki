7679b4a91b18b0eae1fb8b889ee3860d862167a7
Dan <webdevd4n@gmail.com>
2023-05-27 01:27:10 +0100

    Initial navigation created

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <nav className="nav">
+      <div className="nav__inner">
+        <img className="nav__logo " src="./muki-logo.png" alt="Muki Muki"></img>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
